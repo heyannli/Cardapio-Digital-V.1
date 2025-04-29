2ead5595de104278a41fed1e747c19441f2a87f0
heyannli <150404073+heyannli@users.noreply.github.com>
2025-04-29 20:30:30 -0300

    teste inicial

---
+++ script.js
@@ -0,0 +1,492 @@
+const menuData = {
+    bebidas: [
+        {
+            name: "Refrigerante Lata",
+            price: "R$ 8,00",
+            description: "Coca-Cola, Guaraná (Normal e Zero)"
+        },
+        {
+            name: "Água sem Gás",
+            price: "R$ 6,00",
+            description: "Garrafa de 500ml"
+        },
+        {
+            name: "Água com Gás",
+            price: "R$ 7,00",
+            description: "Garrafa de 500ml"
+        },
+        {
+            name: "Suco Del Valle",
+            price: "R$ 10,00",
+            description: "Conferir sabores disponíveis"
+        },
+        {
+            name: "Energético Red Bull",
+            price: "R$ 18,00",
+            description: "Tradicional, Zero e Tropical"
+        },
+
+        {
+            name: "Smirnoff Ice",
+            price: "R$ 17,00",
+            description: ""
+        },
+
+        {
+            name: "Ice 51",
+            price: "R$ 15,00",
+            description: "Conferir sabores disponíveis"
+        },
+
+        {
+            name: "Ice Leev",
+            price: "R$ 15,00",
+            description: "Conferir sabores disponíveis"
+        },
+        {
+            name: "H2O Limoneto",
+            price: "R$ 10,00",
+            description: ""
+        },
+        {
+            name: "Beats",
+            price: "R$ 15,00",
+            description: "Conferir opções disponíveis"
+        },
+    ],
+    cervejas: [
+        {
+            name: "Heineken",
+            price: "R$ 15,00",
+            description: "Long neck (330ml)"
+        },
+        {
+            name: "Budweiser",
+            price: "R$ 12,00",
+            description: "Long neck (330ml)"
+        },
+        {
+            name: "Corona",
+            price: "R$ 15,00",
+            description: "Long neck (330ml)"
+        },
+        {
+            name: "Amstel",
+            price: "R$ 9,00",
+            description: "Lata (350ml)"
+        },
+        {
+            name: "Brahma Duplo Malte",
+            price: "R$ 10,00",
+            description: "Lata (350ml)"
+        },
+        {
+            name: "Original",
+            price: "R$ 9,00",
+            description: "Lata (269ml)"
+        },
+        {
+            name: "Imperio Lager",
+            price: "R$ 12,00",
+            description: "Long neck (330ml)"
+        },
+        {
+            name: "Imperio Gold",
+            price: "R$ 12,00",
+            description: "Long neck (330ml)"
+        },
+        {
+            name: "Imperio Pilsen",
+            price: "R$ 9,00",
+            description: "Long neck (330ml) ou lata (350ml)"
+        },
+        {
+            name: "IPA",
+            price: "R$ 12,00",
+            description: "Lata 350ml"
+        },
+        {
+            name: "Eisenbahn",
+            price: "R$ 9,00",
+            description: "Lata (350ml)"
+        },
+        {
+            name: "Stella Artois",
+            price: "R$ 15,00",
+            description: "Long neck (330ml)"
+        }
+    ],
+    batidas: [
+        {
+            name: "SABORES: Limão -  Maracujá - Abacaxi - Morango",
+            price: "",
+            description: "",
+        },
+        {
+            name: "Caipirinha Pinga",
+            price: "R$ 21,00",
+            description: "Velho Barreiro ou 51"
+        },
+        {
+            name: "Caipiroska Smirnoff",
+            price: "R$ 25,00",
+            description: ""
+        },
+        {
+            name: "Caipiroska Absolut",
+            price: "R$ 30,00",
+            description: ""
+        },
+        {
+            name: "Caipisaque",
+            price: "R$ 26,00",
+            description: ""
+        }
+    ],
+    doses: [
+        {
+            name: "Vodka Smirnoff",
+            price: "R$ 20,00",
+            description: ""
+        },
+        {
+            name: "Gin Nacional",
+            price: "R$ 18,00",
+            description: "Intencion"
+        },
+        {
+            name: "Gin Importado",
+            price: "R$ 25,00",
+            description: "Beefeater, Tanqueray, Gordon's"
+        },
+        {
+            name: "Rum Montilla",
+            price: "R$ 18,00",
+            description: "Carta Ouro"
+        },
+        {
+            name: "Rum Bacardi",
+            price: "R$ 23,00",
+            description: "Carta Branca, Carta Ouro"
+        },
+        {
+            name: "Seleta",
+            price: "R$ 12,00",
+            description: ""
+        },
+        {
+            name: "Salinas",
+            price: "R$ 12,00",
+            description: ""
+        },
+        {
+            name: "Sagatiba",
+            price: "R$ 12,00",
+            description: ""
+        },
+        {
+            name: "Conhaque",
+            price: "R$ 14,00",
+            description: "DomeQ"
+        },
+        {
+            name: "Vodka Absolut",
+            price: "R$ 25,00",
+            description: ""
+        },
+        {
+            name: "Campari",
+            price: "R$ 18,00",
+            description: ""
+        },
+        {
+            name: "Tequila José Cuervo",
+            price: "R$ 25,00",
+            description: "Prata e Ouro"
+        },
+        {
+            name: "Red Label",
+            price: "R$ 30,00",
+            description: ""
+        },
+        {
+            name: "Black Label",
+            price: "R$ 40,00",
+            description: ""
+        },
+        {
+            name: "White Horse",
+            price: "R$ 23,00",
+            description: ""
+        },
+        {
+            name: "Passaport",
+            price: "R$ 20,00",
+            description: ""
+        }
+    ],
+    combos: [
+        {
+            name: "Red Label",
+            price: "R$ 260,00",
+            description: "garrafa fechada + 5 red bull/tônica"
+        },
+        {
+            name: "Black Label",
+            price: "R$ 350,00",
+            description: "garrafa fechada + 5 red bull/tônica"
+        },
+        {
+            name: "Vodka Smirnoff",
+            price: "R$ 210,00",
+            description: "garrafa fechada + 5 red bull/tônica"
+        },
+        {
+            name: "Gin Beefeater",
+            price: "R$ 300,00",
+            description: "garrafa fechada + 5 red bull/tônica"
+        },
+        {
+            name: "Vodka Absolut",
+            price: "R$ 260,00",
+            description: "garrafa fechada + 5 red bull/tônica"
+        },
+        {
+            name: "Balde Heineken",
+            price: "R$ 70,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Budweiser",
+            price: "R$ 54,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Corona",
+            price: "R$ 70,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Amstel",
+            price: "R$ 40,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Brahma Duplo Malte",
+            price: "R$ 40,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Original",
+            price: "R$ 45,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Imperio Pilsen",
+            price: "R$ 40,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Imperio Lager",
+            price: "R$ 54,00",
+            description: "5 unidades"
+        },
+        {
+            name: "Balde Imperio Gold",
+            price: "R$ 54,00",
+            description: "5 unidades"
+        },
+    ],
+    drinks: [
+        {
+            name: "Sex on the Beach",
+            price: "R$ 35,00",
+            description: "Vodka Smirnoff, Peach Stock, suco de laranja e Grenadine"
+        },
+        {
+            name: "Pina Colada",
+            price: "R$ 35,00",
+            description: "Suco de Abacaxi, Rum, Leite de Coco e Creme de Leite || Pode ser não alcoólico",
+        },
+        {
+            name: "Alexander",
+            price: "R$ 35,00",
+            description: "Conhaque, Creme de Cacau, Creme de Leite e Leite Condensado"
+        },
+        {
+            name: "Taça de Gin",
+            price: "R$ 35,00",
+            description: "Gin Importado, Energético/Água Tônica e Fruta"
+        },
+        {
+            name: "Aperol Spritz",
+            price: "R$ 35,00",
+            description: "Aperol, Proseco, Água com Gás e Laranja"
+        },
+        {
+            name: "Cuba Libre",
+            price: "R$ 30,00",
+            description: "Rum, Coca-Cola e Limão"
+        }
+    ],
+    vinhos: [
+        {
+            name: "Vinho Tinto",
+            price: "R$ 80,00",
+            description: "Chateau, Hormiga Negra, Mosquita Negra, Paskua"
+        },
+        {
+            name: "Vinho Branco",
+            price: "R$ 70,00",
+            description: "Barefoot"
+        },
+        {
+            name: "Espumante",
+            price: "R$ 90,00",
+            description: "Conferir as opções do dia"
+        },
+        {
+            name: "Taça de Vinho",
+            price: "R$ 15,00",
+            description: "Vinho Tinto ou Branco"
+        }
+    ],
+    porcoes: [
+        {
+            name: "Calabresa Acebolada",
+            price: "R$ 39,00",
+            description: "acomp: pão fatiado e maionese"
+        },
+        {
+            name: "Batata Frita",
+            price: "R$ 35,00",
+            description: ""
+        },
+        {
+            name: "Batata Frita com Queijo",
+            price: "R$ 38,00",
+            description: ""
+        },
+        {
+            name: "Pastel",
+            price: "R$ 35,00",
+            description: "20 unidades || Carne, Queijo"
+        },
+        {
+            name: "Torresmo Pururuca Duroc Jersey",
+            price: "R$ 39,00",
+            description: ""
+        },
+        {
+            name: "Bolinho Caipira",
+            price: "R$ 40,00",
+            description: "10 unidades || carne, queijo e carne seca"
+        },
+        {
+            name: "Porção The Club",
+            price: "R$ 78,00",
+            description: "calabresa, polenta frita, batata frita | acomp: pão fatiado e maionese"
+        },
+        {
+            name: "Polenta Frita",
+            price: "R$ 35,00",
+            description: ""
+        },
+        {
+            name: "Polenta Frita com Queijo",
+            price: "R$ 40,00",
+            description: ""
+        },
+        {
+            name: "Aneis de Cebola",
+            price: "R$ 48,00",
+            description: ""
+        },
+        {
+            name: "Nuggets",
+            price: "R$ 35,00",
+            description: "15 unidades"
+        },
+        {
+            name: "Bolinho de Feijoada",
+            price: "R$ 40,00",
+            description: "10 unidades"
+        },
+        {
+            name: "Bolinho de Provolone",
+            price: "R$ 40,00",
+            description: "10 unidades"
+        },
+        {
+            name: "Coxinha Cremosa",
+            price: "R$ 40,00",
+            description: "10 unidades"
+        },
+        {
+            name: "Bolinho de Bacalhau",
+            price: "R$ 48,00",
+            description: "10 unidades"
+        },
+        {
+            name: "Bolinho de Camarão",
+            price: "R$ 48,00",
+            description: "10 unidades"
+        },
+        {
+            name: "Bolinho de Mandioca com Costela",
+            price: "R$ 48,00",
+            description: "10 unidades"
+        },
+        {
+            name: "Bolinho de Gorgonzola",
+            price: "R$ 45,00",
+            description: "10 unidades"
+        }
+    ]
+};
+
+// exibir a página inicial
+function showHomePage() {
+    document.getElementById('home-page').style.display = 'block';
+    document.getElementById('category-page').style.display = 'none';
+}
+
+// exibir categoria específica
+function showCategory(categoryId) {
+    document.getElementById('home-page').style.display = 'none';
+
+    const categoryPage = document.getElementById('category-page');
+    categoryPage.style.display = 'block';
+
+    const titles = {
+        bebidas: "<img src='imagens/bebidas.png'> Bebidas",
+        cervejas: "<img src='imagens/cerveja.png'> Cervejas",
+        batidas: "<img src='imagens/batidas.png'> Batidas",
+        doses: "<img src='imagens/dose.png'> Doses",
+        combos: "<img src='imagens/combo.png'> Combos",
+        drinks: "<img src='imagens/drink.png'> Drinks",
+        vinhos: "<img src='imagens/vinho.png'> Vinhos",
+        porcoes: "<img src='imagens/porcoes.png'> Porções",
+    };
+
+    document.getElementById('category-title').innerHTML = titles[categoryId];
+
+    const menuItems = document.getElementById('menu-items');
+        menuItems.innerHTML = '';
+
+    const items = menuData[categoryId];
+    items.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.className = 'menu-item';
+        itemElement.innerHTML = `
+            <div class="item-header">
+                <span class="item-name">${item.name}</span>
+                <span class="item-price">${item.price}</span>
+            </div>
+            <p class="item-description">${item.description}</p>
+        `;
+        menuItems.appendChild(itemElement);
+    });
+}
+
+showHomePage();
